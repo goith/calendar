55448f19ff3da2c6fe1913c631fcc49c819aa61b
gaozhenan <gaozhenan@sogou-inc.com>
2020-05-21 20:33:40 +0800

    update...

--- calendar.go
+++ calendar.go
@@ -64,3 +64,41 @@ func GetMonth() string {
 	//2006-01-02
 	return time.Now().Format("200601")
 }
+
+func GetWeekNumBetweenMonth(a, b string) int {
+	d1, _ := time.Parse("20060102", a+"01")
+	tmp, _ := time.Parse("20060102", b+"01")
+	d2 := GetLastDateOfMonth(tmp)
+
+	days := int(((d2.Sub(d1).Hours())/24 + 1))
+
+	weeks := 0
+	if d1.Weekday() <= time.Wednesday {
+		weeks += 1
+		days -= (6 - int(d1.Weekday()) + 1)
+	}
+
+	if d2.Weekday() >= time.Wednesday {
+		weeks += 1
+		days -= (int(d1.Weekday()) + 1)
+	}
+	weeks += (days / 7)
+
+	return weeks
+}
+
+//获取传入的时间所在月份的第一天，即某月第一天的0点。如传入time.Now(), 返回当前月份的第一天0点时间。
+func GetFirstDateOfMonth(d time.Time) time.Time {
+	d = d.AddDate(0, 0, -d.Day()+1)
+	return GetZeroTime(d)
+}
+
+//获取传入的时间所在月份的最后一天，即某月最后一天的0点。如传入time.Now(), 返回当前月份的最后一天0点时间。
+func GetLastDateOfMonth(d time.Time) time.Time {
+	return GetFirstDateOfMonth(d).AddDate(0, 1, -1)
+}
+
+//获取某一天的0点时间
+func GetZeroTime(d time.Time) time.Time {
+	return time.Date(d.Year(), d.Month(), d.Day(), 0, 0, 0, 0, d.Location())
+}
--- calendar_test.go
+++ calendar_test.go
@@ -13,3 +13,15 @@ func TestGetMondayINWeek(t *testing.T) {
 	day := GetMondayINWeek(w)
 	t.Logf("the date of %s week's Monday is %s", w, day)
 }
+
+func TestGetWeekNumBetweenMonth(t *testing.T) {
+	a := "202004"
+	b := "202004"
+	w := GetWeekNumBetweenMonth(a, b)
+	t.Logf("month %s to %s is %d weeks", a, b, w)
+
+	a = "202004"
+	b = "202005"
+	w = GetWeekNumBetweenMonth(a, b)
+	t.Logf("month %s to %s is %d weeks", a, b, w)
+}
